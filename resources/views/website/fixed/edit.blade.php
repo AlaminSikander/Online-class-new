3e30b50568800c95881e6dc8d19625f871c111cc
Al-Amin Hossain Ridoy <alamin.6534@gmail.com>
2022-04-07 21:12:58 +0600

    lap

--- resources/views/website/fixed/edit.blade.php
+++ resources/views/website/fixed/edit.blade.php
@@ -1,162 +1,139 @@
+<div class="content container-fluid">
 
-<div class="fix">
-      <div class="side-info">
-         <div class="side-info-content">
-            <div class="offset-widget offset-logo mb-40">
-               <div class="row align-items-center">
-                  <div class="col-9">
-                     <a href="index.html">
-                        <img src="assets/img/logo/logo-black.png" alt="Logo">
-                     </a>
-                  </div>
-                  <div class="col-3 text-end"><button class="side-info-close"><i class="fal fa-times"></i></button>
-                  </div>
-               </div>
-            </div>
-            <div class="mobile-menu d-xl-none fix"></div>
-            <div class="offset-widget offset_searchbar mb-30">
-               <div class="menu-search position-relative ">
-                  <form action="#" class="filter-search-input">
-                     <input type="text" placeholder="Search keyword">
-                     <button><i class="fal fa-search"></i></button>
-                  </form>
-               </div>
-            </div>
-
-         </div>
-      </div>
-   </div>
-   <div class="offcanvas-overlay"></div>
-   <div class="offcanvas-overlay-white"></div>
-
-   
-<header class="header-wrap style1">
-    <div class="container">
+    <div class="page-header">
         <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="header-top-left">
-                    <div class="close-header-top xl-none">
-                        <button type="button"><i class="las la-times"></i></button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-            </div>
+            <div class="col">
+                <h3 class="page-title">Student Signup From</h3>
 
+            </div>
         </div>
     </div>
-    </div>
-    <div class="header-bottom py-3 border-bottom border-5" style="background-color: #7F8C8D">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-2 col-md-4 col-6 order-lg-1 order-md-1 order-1">
-                    <!-- <div class="logo">
-                        <a href="index.html"><img src="{{('/img/logo.png')}}" alt="Image"></a>
-                    </div> -->
-                </div>
-                <div class="">
-                    <!-- <div class="col-lg-6 col-md-3 col-6 order-lg-2 order-md-3 order-3"> -->
-                    <div class="main-menu-wrap style1 ">
-                        <div class="menu-close xl-none">
-                            <a href="javascript:void(0)"><i class="las la-times"></i></a>
-                        </div>
-                        <div id="menu" class="text-left d-flex justify-content-around align-items-center">
-                            <div class="logo">
-                                <a href="index.html"><img src="{{url('/img/logo-web.png')}}" alt="Image" width="70px" class="rounded-circle"></a>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{route('user.signup.store')}}" method='POST' enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <h5 class="form-title"><span>Student Information</span></h5>
                             </div>
-                            <ul class="main-menu ">
-                                <li>
-                                    <a href="{{route('website.home')}}">HOME</a>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Student ID</label>
+                                    <input required type="text" name="user_id" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input required type="text" name="name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Image</label>
+                                    <input required name="image" type="file" class="form-control">
+                                </div>
+                            </div>
 
-                                </li>
-                                
-
-                                <li class=""><a href="{{route('website.about')}}">About Us</a>
-                                    
-                                </li>
-                                <li><a href="{{route('website.student.signup')}}">Registration</a></li>
-
-
-
-                                @if(auth()->user() && auth()->user()->role == "student")
-                                    @if(auth()->user())
-                                    <li><a  href="{{route('website.routine')}}">Routine</a></li>
-                                    <li><a href="{{route('website.profile')}}">Profile</a></li>
-                                    <li><a  href="{{route('website.result')}}">Result</a></li>
-                                    <li><a  href="{{route('website.exam')}}">Exam</a></li>
-                                    <li ><a  href="{{route('user.logout')}}">logout</a> </li>
-
-                                    @else
-                                     <li><a href="{{route('user.login')}}">Login</a></li>
-                                          
-                                    @endif
-
-                                @else
-                                    @if(auth()->user())
-                                    <li><a  href="{{route('website.teacherprofile')}}">Profile</a></li>
-                                    <li><a  href="{{route('website.teacher.routine')}}">Routine</a></li>
-                                    <li><a  href="{{route('website.teacher.exam')}}">exam</a></li>
-
-                                    <li><a  href="{{route('website.teacher.result')}}">Result</a></li>
-                                    <li><a  href="{{route('user.logout')}}">logout</a></li>
-
-                                    @else
-                                    <li><a href="{{route('user.login')}}"> User Login</a></li>
-   
-                                    @endif
-
-
-                                @endif
-
-
-                                <li>
-                                    <a href="{{route('admin.login')}}">Admin Login</a>
-
-                                </li>
-
-                                
-
-                            </ul>
-                            </li>
-                            </ul>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Gender</label>
+                                    <select required name="gender" class="form-control select">
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                        <option>Others</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Date of Birth</label>
+                                    <input required name="birth" type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Class</label>
+                                    <select required name="class" class="form-control select">
+                                        <option>Select one</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mobile Number</label>
+                                    <input required name="mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Joining Date</label>
+                                    <input required name="join" type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input required name="email" type="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input required name="password" type="password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <h5 class="form-title"><span>Parent Information</span></h5>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Father's Name</label>
+                                    <input required name="father_name" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Father's Mobile</label>
+                                    <input required name="father_mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mother's Name</label>
+                                    <input required name="mother_name" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mother's Mobile</label>
+                                    <input required name="mother_mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Address</label>
+                                    <input required name="address" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <input hidden value="student" name="role" type="text" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="mobile-bar-wrap">
-                        <div class="sidebar-menu xl-none">
-                            <i class="ri-equalizer-line"></i>
-                        </div>
-                        <div class="mobile-top-bar xl-none">
-                            <i class="ri-settings-3-line"></i>
-                        </div>
-                        <div class="mobile-menu xl-none">
-                            <a href='javascript:void(0)'><i class="ri-menu-line"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-5  order-lg-3 order-md-2">
-                    <div class="header-menu-wrap">
-                        <div class="header-search md-none">
-                            <!-- @if(auth()->user())
-                                <a class="btn btn-primary" href="{{route('user.logout')}}">logout</a>
-                                <a class="btn btn-primary" href="{{route('website.routine')}}">Routine</a>
-                                <a class="btn btn-primary" href="{{route('website.profile')}}">Profile</a>
-                                <a class="btn btn-primary" href="{{route('website.result')}}">Result</a>
-                                @else
-                                    <a href="{{route('user.login')}}">Login</a>
-                            @endif -->
-                        </div>
-
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-    @if(session()->has('error'))
-    <p class="alert alert-danger">{{session()->get('error')}}</p>
-    @endif
-
-    @if(session()->has('msg'))
-    <p class="alert alert-success">{{session()->get('msg')}}</p>
-    @endif
-</header>
+</div>
--- resources/views/website/pages/student_signup.blade.php
+++ resources/views/website/pages/student_signup.blade.php
@@ -12,137 +12,144 @@
     </ul>
 </div>
 @endif
-<div class="container my-5 ">
-    <form action="{{route('user.signup.store')}}" method='POST' enctype="multipart/form-data">
-        @csrf
-        <div class="row">
-            <div class="col-12">
-                <h5 class="form-title"><span>Student Information</span></h5>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Name</label>
-                    <input required name='name' type="text" class="form-control" id="exampleInputPassword1">
-                </div>
-            </div>
+<div class="content container-fluid">
 
-            <div class="col-12 col-sm-6">
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Student ID</label>
-                    <input required name='user_id' type="text" class="form-control">
-                </div>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Gender</label>
-                    <select required name='gender' class="form-control">
-                        <option>Select Gender</option>
-                        <option>Female</option>
-                        <option>Male</option>
-                        <option>Others</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Date of Birth</label>
-                    <div>
-                        <input required name='birth' type="date" class="form-control">
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Class</label>
-                    <select required name='class' type="text" class="form-control">
-                        <option>Select one</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div>
-            </div>
+    <div class="page-header">
+        <div class="row align-items-center">
+            <div class="col">
+                <h3 class="page-title">Student Signup From</h3>
 
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Joining Date</label>
-                    <div>
-                        <input required name='join' type="date" class="form-control">
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Mobile Number</label>
-                    <input required name='mobile' type="text" class="form-control">
-                </div>
-            </div>
-
-
-            <div class="col-12 col-sm-6">
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Email</label>
-                    <input required name='email' type="email" class="form-control" id="exampleInputPassword1">
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6">
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input required name='password' type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-            </div>
-            <div class="col-12">
-                <h5 class="form-title"><span>Parent Information</span></h5>
-            </div>
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Father's Name</label>
-                    <input required name='father_name' type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Father's Mobile</label>
-                    <input required name='father_mobile' type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Mother's Name</label>
-                    <input required name='mother_name' type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Mother's Mobile</label>
-                    <input required name='mother_mobile' type="text" class="form-control">
-                </div>
-            </div>
-
-            <div class="col-12 col-sm-6">
-                <div class="form-group">
-                    <label>Address</label>
-                    <textarea required name='address' class="form-control"></textarea>
-                </div>
-            </div>
-            <div class="mb-3">
-                <label for="formFile" class="form-label">Picture</label>
-                <input name='image' class="form-control" type="file" id="formFile">
-            </div>
-            <div>
-                <input hidden value="student" name="role" type="text" class="form-control">
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </div>
-    </form>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{route('user.signup.store')}}" method='POST' enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <h5 class="form-title"><span>Student Information</span></h5>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Student ID</label>
+                                    <input required type="text" name="user_id" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input required type="text" name="name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Image</label>
+                                    <input required name="image" type="file" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Gender</label>
+                                    <select required name="gender" class="form-control select">
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                        <option>Others</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Date of Birth</label>
+                                    <input required name="birth" type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Class</label>
+                                    <select required name="class" class="form-control select">
+                                        <option>Select one</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mobile Number</label>
+                                    <input required name="mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Joining Date</label>
+                                    <input required name="join" type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input required name="email" type="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input required name="password" type="password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <h5 class="form-title"><span>Parent Information</span></h5>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Father's Name</label>
+                                    <input required name="father_name" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Father's Mobile</label>
+                                    <input required name="father_mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mother's Name</label>
+                                    <input required name="mother_name" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Mother's Mobile</label>
+                                    <input required name="mother_mobile" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Address</label>
+                                    <input required name="address" type="string" class="form-control">
+                                </div>
+                            </div>
+                            <div>
+                                <input hidden value="student" name="role" type="text" class="form-control">
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
